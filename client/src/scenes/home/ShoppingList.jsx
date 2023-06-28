889b00733bbb29dd091498256bd734bad6d709d6
bukani671 <you@example.com>
2023-06-28 21:05:55 +0200

    add

---
+++ client/src/scenes/home/ShoppingList.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Item from "../../components/Item";
+import { Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useDispatch, useSelector } from "react-redux";
+import { setItems } from "../../state";
+
+const ShoppingList = () => {
+    
+  const dispatch = useDispatch();
+  const [value, setValue] = useState("all");
+  const items = useSelector((state) => state.cart.items);
+  const breakPoint = useMediaQuery("(min-width:600px)");
+
+   const handleChange = (event, newValue) => {
+    setValue(newValue);
+};
+}
+
+export default ShoppingList
